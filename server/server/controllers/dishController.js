00d43e37b826fa52650e63870127cf28eeafee7c
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-02-27 09:25:51 +0300

    Dish Controller Hirarechy

---
+++ server/server/controllers/dishController.js
@@ -0,0 +1,21 @@
+
+const dishModel = require("../models/dish");
+
+
+//getting all Restaurants 
+const getAllDishes = async (req, res) => {
+    
+}
+
+
+// getting a specific restaurant
+const getDish = async (req, res) => {
+    
+}
+
+module.exports = {
+    getAllDishes,
+    getDish
+};
+
+
