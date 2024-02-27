04b99d6ac2e4bd5d94304b9d4b453ce568ceb025
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-02-27 09:22:31 +0300

    Restaurant Controller Hiraiechy

---
+++ server/server/controllers/restaurantController.js
@@ -0,0 +1,21 @@
+
+const restaurantModel = require("../models/restaurant");
+
+
+//getting all Restaurants 
+const getAllRestaurants = async (req, res) => {
+    
+}
+
+
+// getting a specific restaurant
+const getRestaurant = async (req, res) => {
+    
+}
+
+module.exports = {
+    getAllRestaurants,
+    getRestaurant
+};
+
+
