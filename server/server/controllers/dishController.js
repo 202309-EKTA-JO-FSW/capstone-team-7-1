6021fcb48cb2bcb50d7bd2fbe696dded42f6df33
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-02-27 09:32:05 +0300

    Updating comments

--- server/server/controllers/dishController.js
+++ server/server/controllers/dishController.js
@@ -2,13 +2,13 @@
 const dishModel = require("../models/dish");
 
 
-//getting all Restaurants 
+//getting all Dishes ( get ) 
 const getAllDishes = async (req, res) => {
     
 }
 
 
-// getting a specific restaurant
+// getting a specific Dish ( get )
 const getDish = async (req, res) => {
     
 }
