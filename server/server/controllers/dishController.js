0c53c3ee1829275b56c487dc79befd8f1868eac2
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-02-27 17:07:53 +0300

    dish controller is done - need check

--- server/server/controllers/dishController.js
+++ server/server/controllers/dishController.js
@@ -4,14 +4,38 @@ const dishModel = require("../models/dish");
 
 //getting all Dishes ( get ) 
 const getAllDishes = async (req, res) => {
-    
+    try {
+        const menu = await dishModel.find({})
+        
+   // if no dishes where found then the is no menu 
+        if (!menu)
+            res.status(422).json("No menu")
+             
+            res.status(200).json(menu)
+
+    }
+    catch {
+        res.status(422).json({error: "unable to fetch menu"})
+    }
 }
 
 
 // getting a specific Dish ( get )
 const getDish = async (req, res) => {
-    
+    const {dishID} = req.params
+    try {
+        const getDishtWithID = await dishModel.findById(dishID)
+        
+        if (!getDishtWithID)
+        res.status(422).json("this Dish is not Found")    
+
+        res.status(200).json(getRestaurantWithID)
+    }
+    catch {
+        res.json({ error: 'unable to fetch restaurant' });
+    }
 }
+
 
 module.exports = {
     getAllDishes,
