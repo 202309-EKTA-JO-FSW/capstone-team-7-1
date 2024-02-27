53a3c66ad0776c062dbabc9faf62295973b40f29
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-02-27 09:30:52 +0300

    Order Controller Hirarechy

---
+++ server/server/controllers/orderController.js
@@ -0,0 +1,33 @@
+
+const orderModel = require("../models/order");
+
+
+//getting order for the user  (get)
+const getUserOrders = async (req, res) => {
+    
+}
+
+
+// creating user order (Post)
+const createOrder = async (req, res) => {
+    
+}
+
+// getting order by ID ( get )
+const getOrder = async (req, res) => {
+    
+}
+
+// deleting order by ID ( delete )
+const deleteOrder = async (req, res) => {
+    
+}
+
+module.exports = {
+    getUserOrders,
+    createOrder,
+    getOrder,
+    deleteOrder
+};
+
+
