80da54f8c6056d78454b2d1b35b0a365d3b24d08
Hakeema ismail <hakeema51.alsoudi@gmail.com>
2024-03-03 12:13:29 +0300

    cartController test done

--- server/server/controllers/cartController.js
+++ server/server/controllers/cartController.js
@@ -76,6 +76,7 @@ const updateCart = async (req, res) => {
         res.status(500).send(error.message);
     }
 };
+
 //*******************************************************************//Done
 // Empty cart
 const emptyCart = async (req, res) => {
