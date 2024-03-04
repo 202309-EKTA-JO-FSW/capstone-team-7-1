25a1830fea0e0bc9bd9dbcf1f07a596544fff820
Ammar_coder87 <ammar.kolko87@gmail.com>
2024-03-04 09:31:10 +0300

    Navbar

---
+++ client/front-end-react/src/App.js
@@ -0,0 +1,18 @@
+
+import { Fragment } from 'react';
+import './App.css';
+import Home from './components/home/Home';
+import Navs from './components/navs/Navs';
+
+function App() {
+  return (
+    <Fragment>
+      <Navs />
+
+      <Home />
+    </Fragment>
+
+  );
+}
+
+export default App;
