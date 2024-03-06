e875d87cb92a312b8e5683c835d78eba20fb5210
Hakeema ismail <hakeema51.alsoudi@gmail.com>
2024-03-07 01:31:08 +0300

    test favorite resturant is done

--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -4,15 +4,14 @@ const router = express.Router();
 
 //const authToken=require('../middlewares/isAuthenticated')
 
-const userController = require("../controllers/userController");
-const restaurantController = require('../controllers/restaurantController');
-const dishController = require('../controllers/dishController');
-const orderController = require('../controllers/orderController');
-const cartController = require('../controllers/cartController');
+// const userController = require("../controllers/userController");
+// const restaurantController = require('../controllers/restaurantController');
+// const dishController = require('../controllers/dishController');
+// const orderController = require('../controllers/orderController');
+// const cartController = require('../controllers/cartController');
 
-const favoriteController = require('../controllers/favoriteController');
-const reviewController = require('../controllers/reviewController');
-
+const favoriteController = require('../controllers/favoriteController');// add
+// const reviewController = require('../controllers/reviewController');
 
 
 // // Public API Endpoints..........
@@ -22,49 +21,48 @@ const reviewController = require('../controllers/reviewController');
 
 
 // here i put the restaurant crud, user can just fetch multi resturants and specfic resturant  //
-router.get('/restaurants', restaurantController.getAllRestaurants);
+// router.get('/restaurants', restaurantController.getAllRestaurants);
 
-router.get('/restaurants/:restaurantID', restaurantController.getRestaurant);
+// router.get('/restaurants/:restaurantID', restaurantController.getRestaurant);
 
 //test purposes
 //router.post('/addRestaurant', restaurantController.addNewRestaurants);
 
 // Add cart routes //
-router.get('/users/:userId/cart', cartController.getUserCart);
-router.post('/users/:userId/cart', cartController.addItemToCart);
+// router.get('/users/:userId/cart', cartController.getUserCart);
+// router.post('/users/:userId/cart', cartController.addItemToCart);
 
-router.put('/users/:userId/cart', cartController.updateCart);
-router.delete('/users/:userId/cart',cartController.emptyCart);
+// router.put('/users/:userId/cart', cartController.updateCart);
+// router.delete('/users/:userId/cart',cartController.emptyCart);
 
 // Add profile route // just fetch and update the profile //
 // router.get('/users/:userId', checkBlackListedToken, userController.getUserProfile);
 // router.put('/users/:userId', checkBlackListedToken, userController.updateUserProfile);
 
 // dish crud
-router.get('/dishes', dishController.getAllDishes);
-router.get('/dishes/:dishID', dishController.getDish);
+// router.get('/dishes', dishController.getAllDishes);
+// router.get('/dishes/:dishID', dishController.getDish);
 
 // test purposes 
 
 //router.post('/addDish', dishController.addNewDish);
 
 // order crud //
-router.get('/users/:userID/orders',  orderController.getUserOrders);
-router.post('/users/:userID/orders', orderController.createOrder);
+// router.get('/users/:userID/orders',  orderController.getUserOrders);
+// router.post('/users/:userID/orders', orderController.createOrder);
 
-router.get('/orders/:orderID',  orderController.getOrder);
-router.delete('/orders/:orderID', orderController.deleteOrder);
+// router.get('/orders/:orderID',  orderController.getOrder);
+// router.delete('/orders/:orderID', orderController.deleteOrder);
 
-// // favorites crud  //
+// Add favorites routes
+router.get('/users/:userId/favorites/restaurants',favoriteController.getUserFavoriteRestaurants);// done 
+router.post('/users/:userId/favorites/restaurants',favoriteController.addFavoriteRestaurant);// done
+router.delete('/users/:userId/favorites/restaurants/:restaurantId',  favoriteController.removeFavoriteRestaurant);// done
 
-// router.get('/users/:userId/favorites/restaurants', authToken, favoriteController.getUserFavoriteRestaurants);
-// router.post('/users/:userId/favorites/restaurants', authToken, favoriteController.addFavoriteRestaurant);
-// router.delete('/users/:userId/favorites/restaurants/:restaurantId', authToken, favoriteController.removeFavoriteRestaurant);
-// router.get('/users/:userId/favorites/dishes', authToken, favoriteController.getUserFavoriteDishes);
-// router.post('/users/:userId/favorites/dishes', authToken, favoriteController.addFavoriteDish);
-// router.delete('/users/:userId/favorites/dishes/:dishId', authToken, favoriteController.removeFavoriteDish);
-
-// // review for user - need check //
+router.get('/users/:userId/favorites/dishes',favoriteController.getUserFavoriteDishes);// done
+router.post('/users/:userId/favorites/dishes', favoriteController.addFavoriteDish);// done
+router.delete('/users/:userId/favorites/dishes/:dishId', favoriteController.removeFavoriteDish);// done
+// // review for user
 
 // router.get('/restaurants/:restaurantId/reviews', reviewController.getRestaurantReviews);
 // router.post('/restaurants/:restaurantId/reviews', authToken, reviewController.addReviewForRestaurant);
