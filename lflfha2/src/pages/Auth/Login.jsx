fc07c8012d7eb4b7f7b1666dcd49c1dc86177ccf
Hakeema ismail <hakeema51.alsoudi@gmail.com>
2024-03-20 14:46:14 +0300

    create profile page in progress

--- lflfha2/src/pages/Auth/Login.jsx
+++ lflfha2/src/pages/Auth/Login.jsx
@@ -24,26 +24,30 @@ const Login = () => {
     <div className="min-h-screen flex items-center justify-center bg-[#FFD5C2]">
       <div className="w-full max-w-md px-8 py-6 bg-white rounded-lg shadow-md">
         {/* Logo Image */}
-              <div className="flex justify-center mb-6">
-                <img src="/LflfhaLogo.png" alt="LFLFhaLogo" className="mb-4 w-50 h-32" />
-                
+              <div className="flex justify-center mb-1">
+                <img src="/LflfhaLogo.png" alt="LFLFhaLogo" className=" w-60 h-32" />
               </div>
 
-        {/* Form Title */}
-        {/* <h1 className="text-lg text-center shadow-md">Welcome in LFLFha account</h1> */}
+
+        <div className="mb-4 mt-2">
+    <h1 className="text-lg   text-center text-orange-700">Access Your LFLFha Account</h1>
+  </div>
 
         {/* Sign-in form */}
         <form onSubmit={handleSubmit}>
+
           {/* Email input */}
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-bold mb-2">Email</label>
             <input type="email" id="email" name="email" placeholder="Enter your email" value={credentials.email} onChange={handleChange} required className="w-full px-3 py-2 border rounded" />
           </div>
+
           {/* Password input */}
           <div className="mb-4">
             <label htmlFor="password" className="block text-sm font-bold mb-2">Password</label>
             <input type="password" id="password" name="password" placeholder="Enter your password" value={credentials.password} onChange={handleChange} required className="w-full px-3 py-2 border rounded" />
           </div>
+          
           {/* Submit button */}
           <div className="flex items-center justify-center">
             <button type="submit" className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign In</button>
