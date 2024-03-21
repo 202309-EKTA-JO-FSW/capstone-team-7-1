46e2a27afc6825c134febc36f028960f7803e685
Hakeema ismail <hakeema51.alsoudi@gmail.com>
2024-03-21 18:13:28 +0300

    About page done successfully

--- lflfha2/src/components/TeamSection.js
+++ lflfha2/src/components/TeamSection.js
@@ -45,19 +45,11 @@ const TeamSection = () => {
               alt={member.name}
               className="rounded-full w-32 h-32 mb-4"
             />
-            <h3 className="text-lg font-bold">{member.name}</h3>
-            <p className="text-sm mb-4">{member.role}</p>
+            <h3 className="text-lg font-bold mb-2 ">{member.name}</h3>
+            <p className="text-sm mb-6 ">{member.role}</p>
             <div className="flex justify-center gap-4 hover:underline">
-              <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
-                {/* Replace with actual LinkedIn icon */}
-                
-
-                LinkedIn  
-              </a >
-              <a href={member.github} target="_blank" rel="noopener noreferrer">
-                {/* Replace with actual GitHub icon */}
-                GitHub
-              </a>
+              <a href={member.linkedin} target="_blank" rel="noopener noreferrer"> LinkedIn </a >
+              <a href={member.github} target="_blank" rel="noopener noreferrer"> GitHub </a>
             </div>
           </div>
         ))}
@@ -67,7 +59,6 @@ const TeamSection = () => {
 };
 
 export default TeamSection;
-
 
 
 
