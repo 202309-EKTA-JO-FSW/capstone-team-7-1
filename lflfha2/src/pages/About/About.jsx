799549e1a53809d6d81a821790a6ccfd4e71b802
Hakeema ismail <hakeema51.alsoudi@gmail.com>
2024-03-21 17:30:20 +0300

    About page done successfully

--- lflfha2/src/pages/About/About.jsx
+++ lflfha2/src/pages/About/About.jsx
@@ -11,27 +11,28 @@ const AboutPage = () => {
 
       <div className="flex flex-wrap justify-center items-center -mx-4">
         <div className="px-4 flex-1">
-          <div className="max-w-sm mx-auto mb-20">
+          <div className="max-w-sm mx-auto mb-10">
             <Image
-              src="/foode.webp"
-              alt="Founders"
-              width={400} 
-              height={300}
+              src="/img/developers-team.png"
+              alt="developers-team"
+              width={800} 
+              height={800}
               objectFit="cover" 
             />
           </div>
         </div>
-        <div className="px-4 flex-1">
-          <h2 className="text-3xl font-semibold mb-4">About Us</h2>
-          <p className="text-justify leading-relaxed ">
-            Discover Lflfha,where every click brings you closer to a culinary adventure.
-            Designed with passion by the full-stack developers from Re:coded Bootcamp,
+        <div className="px-4 flex-2 ">
+          <h2 className="text-3xl text-center font-semibold mb-4">About Us</h2>
+          <p 
+          className="text-secondary text-lg text-center mb-10 p-8">
+            Lflfha website designed with passion by the full-stack developers from Re:coded Bootcamp,
             this web application takes you on a journey through a virtual bazaar, rich
             in flavors and culture. Leveraging dynamic JavaScript frameworks,
             we've created an engaging, user-friendly experience that not only
             demonstrates our technical prowess but also our dedication to
             creating memorable moments for our users.
-          </p>
+            {/* <span className="last-line">creating memorable moments for our users.</span> */}
+</p>
         </div>
       </div>
       <TeamSection/> 
